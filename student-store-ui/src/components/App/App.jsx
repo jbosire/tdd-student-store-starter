377e595e4087cc2787aed30d18b5b8aa64c4e35d
Joram Bosire <jbosire@jbosire-ltm73m6.internal.salesforce.com>
2022-06-22 09:24:44 -0700

    Update handlers functionality

--- student-store-ui/src/components/App/App.jsx
+++ student-store-ui/src/components/App/App.jsx
@@ -6,10 +6,120 @@ import ProductDetail from "../ProductDetail/ProductDetail"
 import NotFound from "../NotFound/NotFound"
 import "./App.css"
 import {BrowserRouter, Route, Routes} from "react-router-dom"
+import {useState} from "react"
+import { useEffect } from "react"
 
 
 
 export default function App() {
+  const [products,setProducts] = useState([])
+  const [isFetching,setIsFetching] = useState(null)
+  const [error,setError] = useState(null)
+  const [isOpen,setIsOpen] = useState(null)
+  const [shoppingCart,setShoppingCart] = useState([])
+  const [checkoutForm,setcheckoutForm] = useState(null)
+
+  
+
+  useEffect(async () => {
+
+    let url = `https://codepath-store-api.herokuapp.com/store`;
+    console.log(url);
+
+    try{
+        let response = await fetch(url);
+        console.log("response is: ", response);
+        let responseData = await response.json();
+        console.log("responseData is: ", responseData);
+        setProducts(responseData);
+       
+        
+    }
+    catch(e){
+        console.log(e);
+        setError(e);
+    }
+  }, []);
+
+  const handleAddItemToCart = (productId) => {
+    var newItem;
+    var newCart = [];
+
+    for(var i=0; i < shoppingCart.length; i++){
+      if(shoppingCart[i].itemId === productId){
+        newItem = {
+          itemId: productId,
+          quantity: shoppingCart[i].quantity + 1,
+
+
+        }
+
+        newCart.concat(newItem);
+      } else if(i === shoppingCart.length - 1){
+        newItem = {
+          itemId: productId,
+          quantity: 1,
+        }
+
+        newCart.concat(newItem);
+
+      }
+
+    }
+
+    setShoppingCart(newCart);
+
+  
+  }
+
+  const handleRemoveItemFromCart = (productId) => {
+    var newItem;
+    var newCart = [];
+
+    for(var i=0; i < shoppingCart.length; i++){
+      if(shoppingCart[i].itemId === productId){
+        var newQty = shoppingCart[i].quantity - 1;
+        if(newQty > 0){
+        newItem = {
+          itemId: productId,
+          quantity: newQty,
+
+
+        }
+        
+        newCart.concat(newItem);
+       }
+      } 
+    }
+
+    setShoppingCart(newCart);
+
+
+  
+  }
+
+  const handleOnToggle = () => {
+    if(isOpen){
+      setIsOpen(false);
+    }
+    else{
+      setIsOpen(true);
+    }
+  
+  }
+
+  const handleOnCheckoutFormChange = () => {
+
+
+  
+  }
+
+  const handleOnSubmitCheckoutForm = () => {
+
+  
+  }
+
+
   return (
     <div className="app">
       <BrowserRouter>
@@ -19,7 +129,7 @@ export default function App() {
         <main>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/products/:productId" element={<ProductDetail />} />
+          <Route path="/products/:productId" element={<ProductDetail handleAddItemToCart={() => handleAddItemToCart()} handleRemoveItemFromCart={() => handleRemoveItemFromCart()}/>} />
           <Route path="*" element={<NotFound />} />
 
         </Routes>
--- student-store-ui/src/components/ProductDetail/ProductDetail.jsx
+++ student-store-ui/src/components/ProductDetail/ProductDetail.jsx
@@ -2,11 +2,14 @@ import * as React from "react"
 import "./ProductDetail.css"
 import Sidebar from "../Sidebar/Sidebar"
 import Navbar from "../Navbar/Navbar"
+import {useState} from "react"
 
-export default function ProductDetail() {
+export default function ProductDetail(props) {
+    const [product,setProduct] = useState(null)
+    
     return (
-    <div className="produce">
-        <p>Produce</p>
+    <div className="product-detail">
+        
         <Navbar />
         <Sidebar />
         </div>
