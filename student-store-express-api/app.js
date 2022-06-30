b4252569c33dcd50e65d3c40ad9966fad741feb6
Joram Bosire <jbosire@jbosire-ltm73m6.internal.salesforce.com>
2022-06-30 11:27:32 -0700

    Removed unnecessary comments

--- student-store-express-api/app.js
+++ student-store-express-api/app.js
@@ -6,7 +6,6 @@ const router = require("./routes/store")
 const cors = require("cors")
 app.use(cors())
 
-//const { NotFoundError } = require('./utils/errors')
 
 app.use(morgan("tiny"))
 app.use(express.json())
@@ -20,19 +19,6 @@ app.use("/store", router)
 
 
 
-// app.use((req,res,next) =>{
-//     return next(new NotFoundError("Not found"))
-// })
-
-// app.use((error,req,res,next) => {
-//     const status = error.status || 500;
-//     const message = error.message 
-
-//     return res.status(status).json({
-//         error:{message,status}
-//     })
-                                                                                                                                                                                                                                                    
-// })
 
 
 
